3e691e19aad51892e8d18a7174d825abd8befeee
Matthew Sigouin <104263329+Sigouin@users.noreply.github.com>
2024-02-22 15:57:58 -0500

    Fixed sidebar navigation arrow (#3)

--- features/layout/sidebar-navigation/sidebar-navigation.tsx
+++ features/layout/sidebar-navigation/sidebar-navigation.tsx
@@ -91,6 +91,7 @@ export function SidebarNavigation() {
               isCollapsed={isSidebarCollapsed}
               onClick={() => toggleSidebar()}
               className={styles.collapseMenuItem}
+              flipped={isSidebarCollapsed}
             />
           </ul>
         </nav>
