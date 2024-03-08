3fefff7fa59c66ad19ae0c7aa317f241f8b3d2b1
Matthew <matt@sigouin.codes>
2024-03-08 07:35:21 -0500

    Refactored Button component to match figma specs

--- features/ui/button/button.tsx
+++ features/ui/button/button.tsx
@@ -1,9 +1,63 @@
-import { ButtonHTMLAttributes } from "react";
+import React, { ButtonHTMLAttributes } from "react";
 import classNames from "classnames";
 import styles from "./button.module.scss";
 
-export function Button(props: ButtonHTMLAttributes<HTMLButtonElement>) {
+export enum ButtonSize {
+  sm = "sm",
+  md = "md",
+  lg = "lg",
+  xl = "xl",
+}
+
+export enum ButtonColor {
+  primary = "primary",
+  secondary = "secondary",
+  gray = "gray",
+  empty = "empty",
+  emptyGray = "emptyGray",
+  error = "error",
+  emptyError = "emptyError",
+}
+
+export type ButtonIcon = React.ReactNode;
+
+export enum ButtonIconPosition {
+  leading = "leading",
+  trailing = "trailing",
+}
+
+type ButtonProps = {
+  children: React.ReactNode;
+  size?: ButtonSize;
+  color?: ButtonColor;
+  icon?: ButtonIcon;
+  iconPosition?: ButtonIconPosition | string;
+} & ButtonHTMLAttributes<HTMLButtonElement>;
+
+export function Button({
+  children,
+  icon,
+  size,
+  color,
+  iconPosition,
+  ...props
+}: ButtonProps) {
+  const hasIcon = icon ? "hasIcon" : "";
+
   return (
-    <button {...props} className={classNames(styles.button, props.className)} />
+    <button
+      {...props}
+      className={classNames(
+        styles.button,
+        props.className,
+        styles[size || ""],
+        styles[color || ""],
+        styles[hasIcon],
+      )}
+    >
+      {iconPosition === "leading" && icon}
+      {children}
+      {iconPosition === "trailing" && icon}
+    </button>
   );
 }
