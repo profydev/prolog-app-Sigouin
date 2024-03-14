9e55d663c561689a6d4cc0b5a8d3151585a4b36e
Matthew <matt@sigouin.codes>
2024-03-14 08:53:34 -0400

    Checkbox created

--- features/ui/checkbox/checkbox.tsx
+++ features/ui/checkbox/checkbox.tsx
@@ -1,6 +1,6 @@
-import React, { InputHTMLAttributes } from "react";
-// import styles from "./checkbox.module.scss";
-// import classNames from "classnames";
+import { useRef, useEffect } from "react";
+import styles from "./checkbox.module.scss";
+import classNames from "classnames";
 
 export enum CheckboxSize {
   small = "small",
@@ -10,13 +10,55 @@ export enum CheckboxSize {
 type CheckboxProps = {
   label?: string;
   size?: CheckboxSize;
-} & InputHTMLAttributes<HTMLInputElement>;
+  checked?: boolean;
+  indeterminate?: boolean;
+  disabled?: boolean;
+  autoFocus?: boolean;
+  onChange?: (checked: boolean) => void;
+};
 
-export function Checkbox({ label }: CheckboxProps) {
+export function Checkbox({
+  label,
+  size,
+  onChange,
+  checked,
+  indeterminate,
+  disabled,
+  ...props
+}: CheckboxProps) {
+  const checkboxRef = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    if (checkboxRef.current) {
+      checkboxRef.current.indeterminate = indeterminate || false;
+    }
+  }, [indeterminate]);
+
+  const handleChange = () => {
+    const newChecked = !checked;
+    if (onChange) {
+      onChange(newChecked);
+    }
+  };
+
   return (
-    <label>
-      <input type="checkbox" />
-      {label && <span>{label}</span>}
+    <label
+      className={classNames(styles.formControl, {
+        [styles.disabledLabel]: disabled,
+      })}
+    >
+      <input
+        type="checkbox"
+        checked={checked}
+        onChange={handleChange}
+        ref={checkboxRef}
+        disabled={disabled}
+        {...props}
+        className={classNames(styles.checkboxInput, styles[size || ""], {
+          [styles.indeterminate]: indeterminate,
+        })}
+      />
+      {label}
     </label>
   );
 }
