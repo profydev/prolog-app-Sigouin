e2427a87760f3c23f212e00350d09d03a7d3a7ea
Matthew <matt@sigouin.codes>
2024-03-27 18:41:47 -0400

    Added additional routes for header

--- config/routes.tsx
+++ config/routes.tsx
@@ -5,4 +5,7 @@ export const Routes = {
   alerts: "/dashboard/alerts",
   users: "/dashboard/users",
   settings: "/dashboard/settings",
+  pricing: "/pricing",
+  products: "/products",
+  documentation: "/documentation",
 };
