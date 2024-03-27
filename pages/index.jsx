2acf53b45019401f9639870bdc0dcd79fc9865db
Matthew Sigouin <104263329+Sigouin@users.noreply.github.com>
2024-03-27 19:41:27 -0400

    Landing page header (#15)
    * Added additional routes for header
    
    * Added map for nav links
    
    * added styling for nav and dashboard link

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,13 +1,34 @@
 import { Routes } from "@config/routes";
 import styles from "./index.module.scss";
 
+const navigationLinks = [
+  { text: "Home", link: Routes.home },
+  { text: "Products", link: Routes.products },
+  { text: "Documentation", link: Routes.documentation },
+  { text: "Pricing", link: Routes.pricing },
+];
+
 const IssuesPage = () => {
   return (
     <div>
       <header className={styles.header}>
         {/* eslint-disable-next-line @next/next/no-img-element */}
         <img src="/icons/logo-large.svg" alt="Prolog logo" />
-        <a href={Routes.projects}>Dashboard</a>
+
+        <nav className={styles.navLinks}>
+          <ul>
+            {navigationLinks.map((item) => {
+              return (
+                <li key={item.text}>
+                  <a href={item.link}>{item.text}</a>
+                </li>
+              );
+            })}
+          </ul>
+        </nav>
+        <a href={Routes.projects} className={styles.dashboardLink}>
+          Open Dashboard
+        </a>
       </header>
       <button
         className={styles.contactButton}
