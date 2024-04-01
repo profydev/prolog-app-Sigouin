48b5bb5f6df0f49765302a7398ee646ab7896a71
Matthew <matt@sigouin.codes>
2024-04-01 16:18:02 -0400

    Made urls absolute

--- api/content.ts
+++ api/content.ts
@@ -3,7 +3,35 @@ import type { Page } from "./content.types";
 
 const ENDPOINT = "/content-page/slug";
 
+function setAbsoluteUrl<T>(data: T): T {
+  if (!data) {
+    return data;
+  }
+
+  if (typeof data === "string" && data.startsWith("/")) {
+    return `${process.env.NEXT_PUBLIC_API_BASE_URL}${data}` as unknown as T;
+  }
+
+  if (Array.isArray(data)) {
+    return data.map(setAbsoluteUrl) as unknown as T;
+  }
+
+  if (typeof data === "object") {
+    return Object.keys(data).reduce(
+      (acc, key) => {
+        const value = (data as Record<string, unknown>)[key];
+        return {
+          ...acc,
+          [key]: setAbsoluteUrl(value),
+        };
+      },
+      {} as unknown as T,
+    );
+  }
+  return data;
+}
+
 export async function getContentPage(slug: string) {
   const { data } = await axios.get<Page>(ENDPOINT.replace("slug", slug));
-  return data;
+  return setAbsoluteUrl(data);
 }
