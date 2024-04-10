735e14e54b8c6224dbf3dc088edada9a04ca1f8f
Matthew <matt@sigouin.codes>
2024-04-10 06:47:38 -0400

    Fixing svg issue

--- features/ui/customselect/customselect.tsx
+++ features/ui/customselect/customselect.tsx
@@ -52,9 +52,9 @@ const CustomSelectOption = (props: OptionProps<OptionType>) => (
             <path
               d="M16.6668 5L7.50016 14.1667L3.3335 10"
               stroke="#7F56D9"
-              stroke-width="1.66667"
-              stroke-linecap="round"
-              stroke-linejoin="round"
+              strokeWidth="1.66667"
+              strokeLinecap="round"
+              strokeLinejoin="round"
             />
           </svg>
         </span>
